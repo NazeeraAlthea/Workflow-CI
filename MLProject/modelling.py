01e528a595e749f3632101627be8f3f6143432bf
NazeeraAlthea <ryamaulana2894@gmail.com>
2025-12-23 19:15:47 +0700

    ml flow run

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -42,47 +42,52 @@ grid_search = GridSearchCV(
     n_jobs=-1
 )
 
-with mlflow.start_run(run_name="LogReg_Tuning"):
-    grid_search.fit(X_train, y_train)
-
-    best_model = grid_search.best_estimator_
-    y_pred = best_model.predict(X_test)
-
-    acc = accuracy_score(y_test, y_pred)
-    prec = precision_score(y_test, y_pred, average="macro")
-    rec = recall_score(y_test, y_pred, average="macro")
-    f1 = f1_score(y_test, y_pred, average="macro")
-
-    mlflow.log_params(grid_search.best_params_)
-    mlflow.log_metric("accuracy", acc)
-    mlflow.log_metric("precision", prec)
-    mlflow.log_metric("recall", rec)
-    mlflow.log_metric("f1_score", f1)
-
-    mlflow.sklearn.log_model(best_model, "model")
-
-    print("Best Params:", grid_search.best_params_)
-    print("Accuracy:", acc)
+if mlflow.active_run() is None:
+    mlflow.start_run(run_name="LogReg_Tuning")
     
-    # Confusion Matrix
-    cm = confusion_matrix(y_test, y_pred)
+grid_search.fit(X_train, y_train)
 
-    plt.figure(figsize=(6, 4))
-    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
-    plt.xlabel("Predicted")
-    plt.ylabel("Actual")
+best_model = grid_search.best_estimator_
+y_pred = best_model.predict(X_test)
 
-    cm_path = "confusion_matrix.png"
-    plt.savefig(cm_path)
-    plt.close()
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred, average="macro")
+rec = recall_score(y_test, y_pred, average="macro")
+f1 = f1_score(y_test, y_pred, average="macro")
 
-    mlflow.log_artifact(cm_path)
+mlflow.log_params(grid_search.best_params_)
+mlflow.log_metric("accuracy", acc)
+mlflow.log_metric("precision", prec)
+mlflow.log_metric("recall", rec)
+mlflow.log_metric("f1_score", f1)
 
-    # Classification Report
-    report = classification_report(y_test, y_pred)
+mlflow.sklearn.log_model(best_model, "model")
 
-    report_path = "classification_report.txt"
-    with open(report_path, "w") as f:
-        f.write(report)
+print("Best Params:", grid_search.best_params_)
+print("Accuracy:", acc)
 
-    mlflow.log_artifact(report_path)
+# Confusion Matrix
+cm = confusion_matrix(y_test, y_pred)
+
+plt.figure(figsize=(6, 4))
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+
+cm_path = "confusion_matrix.png"
+plt.savefig(cm_path)
+plt.close()
+
+mlflow.log_artifact(cm_path)
+
+# Classification Report
+report = classification_report(y_test, y_pred)
+
+report_path = "classification_report.txt"
+with open(report_path, "w") as f:
+    f.write(report)
+
+mlflow.log_artifact(report_path)
+
+if mlflow.active_run() is not None:
+    mlflow.end_run()
